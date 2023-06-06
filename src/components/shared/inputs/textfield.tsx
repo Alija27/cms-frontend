3314206a96a234dd4fab9b74bcbb1c5cc028a54a
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-06 16:17:37 +0545

    created component TextField

--- src/components/shared/inputs/textfield.tsx
+++ src/components/shared/inputs/textfield.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-const textfield = (props:any) => {
-    const props={
-        id,
-        name,
-        placeholder,
-        type,
-        required,
+type TextProps = {
+  label:string;
+  name: string;
+  placeholder: string;
+  type: string;
+  required: boolean;
 
-    };
+}
+
+const TextField = (props: TextProps) => {
+  const { label,name, placeholder, type, required } = props;
   return (
-<>
-<input id={name} name={name} placeholder={placeholder} type={type} required={required} className="p-5 m-5" />
-</>
+    <>
+      <label htmlFor={name} className="block text-gray-600 font-semibold text-xl  text-md  mb-2">{label}</label>
+      <input id={name} name={name} placeholder={placeholder} type={type} required={required} className="p-4 border border-gray-200 text-gray-700 leading-tight text-xl rounded-md w-full shadow focus:bg-white focus:outline-none focus:border-blue-400" />
+    </>
   )
 }
 
-export default textfield
+export default TextField
