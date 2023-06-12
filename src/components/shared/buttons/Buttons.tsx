b25244241e21a5a1246a31d4592169d51a398e48
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-12 13:20:07 +0545

    button component created

--- src/components/shared/buttons/Buttons.tsx
+++ src/components/shared/buttons/Buttons.tsx
@@ -9,7 +9,7 @@ const Buttons = (props: ButtonProps) => {
     const { text, type, className } = props;
     return (
         <div>
-            <button type={type} className={`bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium ${className}`}>{text}</button>
+            <button type={type} className={`bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 px-3 py-1 text-white  rounded-md dark:focus:ring-blue-800 font-medium ${className}`}>{text}</button>
             {/*         <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Blue</button>
  */}    </div>
     )
