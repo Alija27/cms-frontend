cc051313899e27e4346f3eedab3852946ee774de
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-16 15:35:05 +0545

    Dashboard layout completed

--- src/components/shared/dashboard/Layout.tsx
+++ src/components/shared/dashboard/Layout.tsx
@@ -3,26 +3,21 @@ import Sidenav from './Sidenav'
 import Dashnav from './Dashnav'
 import TextFields from '../inputs/TextFields'
 import Buttons from '../buttons/Buttons'
+import Dashboard from './Dashboard'
 
-const Layout = () => {
+const Layout = ({children}:any) => {
   return (
     <>
-     <div className="">
-     <Sidenav/>
-     <Dashnav/>
-     <div className=" h-screen ">
-        <div className="bg-white absolute top-16 lg:left-[17%] h-screen lg:mx-0 w-full lg:w-10/12 z-0">
-            <div className=" m-5 bg-white border text-black p-5 h-auto">
-                <div className="flex justify-between p-1 m-1 border-b items-baseline">
-                    <div className="text-2xl text-gray-800 font-semibold">Users</div>
-                    <div className=""><Buttons type="button" text="Add New" /></div>
-                </div>
-           <TextFields type="text" label="Name" placeholder="Name" required={true} name="name"/>
-            </div>
+      <div className="flex">
+        <Sidenav />
+        <div className='w-full'>
+          <Dashnav />
+          <div className="h-[calc(100vh-60px)] p-8 text-justify overflow-y-scroll">
+            {children}
+          </div>
         </div>
-     </div>
-     
-     </div>
+
+      </div>
     </>
   )
 }
