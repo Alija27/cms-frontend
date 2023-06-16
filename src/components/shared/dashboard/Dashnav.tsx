29b5f3236b68d515c0c8fdab85fa2d789e0c951b
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-16 15:36:03 +0545

    Dashboard navbar updated

--- src/components/shared/dashboard/Dashnav.tsx
+++ src/components/shared/dashboard/Dashnav.tsx
@@ -3,8 +3,10 @@ import logo from '../../../assets/LICTlogo.png'
 const Dashnav = () => {
   return (
     <>
-
-      <div className=" lg:w-10/12 bg-white px-2  absolute lg:left-[16.8%] shadow py-3 w-full flex justify-end">
+      <div className="bg-white px-2 shadow py-3 w-full flex justify-between">
+        <div>
+Menu
+        </div>
         <div className="rounded-full bg-blue-500 h-8 w-8 p-2 text-center text-white border-2 border-gray-200">
           <img src={logo} />
         </div>
