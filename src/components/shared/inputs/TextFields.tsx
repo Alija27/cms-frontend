a3ba67918b272e1c4c19414c9d3bdb7acfc7a32f
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-22 20:34:48 +0545

    register added in field

--- src/components/shared/inputs/TextFields.tsx
+++ src/components/shared/inputs/TextFields.tsx
@@ -7,19 +7,22 @@ type TextProps = {
   type: string;
   required?: boolean;
   className?: string;
+  register?:any;
   error?: string;
 }
 
 const TextFields = (props: TextProps) => {
-  const { label, name, placeholder, type, required, className, error } = props;
+  const { label, name, placeholder, type, required, className,register, error } = props;
 
   return (
     <>
-      <label htmlFor={name} className="block text-gray-600 font-semibold text-md  text-md  mb-2">{label}</label>
-      <input id={name} name={name} placeholder={placeholder} type={type} required={required} className={`p-2 border border-gray-200 text-gray-700 leading-tight text-md rounded-md w-full shadow focus:bg-white focus:outline-none focus:border-blue-400 ${className}`} />
+   <div className="flex flex-col gap-1">
+      <label htmlFor={name} className="block text-gray-600  text-md font-md ">{label} {required && <span className="text-red-500">*</span>}</label>
+      <input id={name} name={name} placeholder={placeholder} type={type} required={required} className={`p-2 border border-gray-200 text-gray-700 leading-tight text-md rounded-md w-full shadow focus:bg-white focus:outline-none focus:border-blue-400 ${className}`} {...(register&&register(name))} />
       {error && (
         <div className="text-red-500">{error}</div>
       )}
+      </div>
     </>
   )
 }
