dc0d19004da9cffa4fa36bb4ccfd12f2a75d127b
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-22 20:28:48 +0545

    added AuthSlice in reducer

--- src/app/rootReducer.tsx
+++ src/app/rootReducer.tsx
@@ -1,7 +1,8 @@
 import {combineReducers} from "@reduxjs/toolkit"
+import  AuthSlice  from "./feature/Auth/AuthSlice";
 
 const rootReducer = combineReducers({
-	
+    AuthSlice:AuthSlice,
 });
 
 export default rootReducer;
