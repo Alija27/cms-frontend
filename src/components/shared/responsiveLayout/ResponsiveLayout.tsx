4353a8cf416727ecd777701b15d131bf4b4cc585
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-22 20:35:15 +0545

    Layout created

---
+++ src/components/shared/responsiveLayout/ResponsiveLayout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../../../assets/LICTlogo.png"
+
+const ResponsiveLayout = ({children}:any) => {
+  return (
+    <div>
+        <div className="bg-white h-[92vh] flex flex-col justify-center items-center shadow-lg">
+			<div className="bg-white p-8 space-y-4 w-10/12 md:w-6/12 lg:w-4/12 rounded-lg shadow-lg border">
+				<div className="flex justify-center items-center">
+					<img src={logo} className=" "/>
+				</div>
+				<div>{children}</div>
+			</div>
+		</div>
+    </div>
+  )
+}
+
+export default ResponsiveLayout
