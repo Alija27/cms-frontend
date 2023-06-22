621d29b95872f1d7b8b50ce7414a94c499463d40
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-22 20:30:36 +0545

    Navlink added

--- src/components/landingPage/navbar/Navbar.tsx
+++ src/components/landingPage/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from "../../../assets/LICTlogo.png"
 import { FaBars } from "react-icons/fa"
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 type Link = {
   text: string,
@@ -43,28 +44,28 @@ const Navbar = () => {
       <div className="absolute top-20 z-0">hello</div> */}
 
 
-      <div className="bg-blue-500 text-white p-2 font-bold">9845984853</div>
-      <nav className="flex justify-between items-center px-8 shadow-lg border-2 border-gray-200 py-1">
+      {/* <div className="bg-blue-500 text-white p-2 font-bold">9845984853</div> */}
+      <nav className="flex justify-between items-center px-8 shadow-lg border border-gray-100 py-1">
 
-        <span><img src={logo} /></span>
+        <span><img src={logo} className="h-10"/></span>
 
         <span className="flex">
           <ul className="flex space-x-8 text-center invisible lg:visible">
             {links.map((link, index) =>
-              <a key={index} href={link.url} > <li className={` mt-2 lg:mt-0 border-b-4 border-white  transition-colors duration-300 ease-in-out  hover:text-blue-500 hover:border-blue-500 rounded border-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:text-gradient-to-br ${link.className}`}>{link.text}</li> </a>
+              <NavLink key={index} to={link.url} > <li className={` mt-2 lg:mt-0 border-b-4 border-white  transition-colors duration-300 ease-in-out  hover:text-blue-500 hover:border-blue-500 rounded border-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:text-gradient-to-br ${link.className}`}>{link.text}</li> </NavLink>
             )}
           </ul>
 
           <span className="lg:hidden" onClick={toggleMenu}>  <FaBars />  </span>
         </span>
       </nav>
-      <ul className={`bg-white shadow-md flex flex-col absolute  w-full lg:-top-96 lg:hidden text-center    ${isMenuOpen ? "top-28" : " -top-96"} transistion-all duration-500 ease-in-out `}>
+      <ul className={`bg-white shadow-md flex flex-col absolute  w-full lg:-top-96 lg:hidden text-center    ${isMenuOpen ? "top-12" : " -top-96"} transistion-all duration-500 ease-in-out `}>
         {links.map((link, index) =>
-          <a key={index} href={link.url} > <li className={`mx-5 px-5 p-2 mt-2 lg:mt-0 border-b-4  border-white  transition-colors duration-300 ease-in-out  hover:text-blue-500 hover:border-blue-500 rounded border-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:text-gradient-to-br ${link.className}`}>{link.text}</li> </a>
+          <NavLink key={index} to={link.url} > <li className={`mx-5 px-5 p-2 mt-2 lg:mt-0 border-b-4  border-white  transition-colors duration-300 ease-in-out  hover:text-blue-500 hover:border-blue-500 rounded border-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:text-gradient-to-br ${link.className}`}>{link.text}</li> </NavLink>
         )}
       </ul>
 
-      
+
     </>
   )
 }
