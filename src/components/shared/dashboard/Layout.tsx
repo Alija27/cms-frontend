001bb6578f20dfb56b0c841f06e9d2cb3dda9e12
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-22 20:32:00 +0545

    Added condition for rendering  Navlink

--- src/components/shared/dashboard/Layout.tsx
+++ src/components/shared/dashboard/Layout.tsx
@@ -4,12 +4,20 @@ import Dashnav from './Dashnav'
 import TextFields from '../inputs/TextFields'
 import Buttons from '../buttons/Buttons'
 import Dashboard from './Dashboard'
+import { DNavLinks, adminNavLinks } from '../../../constants/Constants'
+import { useAppSelector } from '../../../app/hooks'
 
 const Layout = ({children}:any) => {
+  const authState=useAppSelector((store)=>store.AuthSlice);
+  let navlinks:DNavLinks[]=[];
+  navlinks=adminNavLinks;
+  if(authState.current_user && authState.current_user.roles.includes("admin")){
+    navlinks=adminNavLinks;
+  }
   return (
     <>
       <div className="flex">
-        <Sidenav />
+        <Sidenav navlinks={navlinks}/>
         <div className='w-full'>
           <Dashnav />
           <div className="h-[calc(100vh-60px)] p-8 text-justify overflow-y-scroll">
