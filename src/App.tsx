6cbe24145dc6fd90cdf5a32601a6804f543114ea
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-25 20:59:34 +0545

    added Allroute and BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './components/landingPage/navbar/Navbar'
+import Home from './components/landingPage/pages/Home'
+import Sidenav from './components/shared/dashboard/Sidenav'
+import Dashnav from './components/shared/dashboard/Dashnav'
+import Layout from './components/shared/dashboard/Layout'
+import { Table, TableActions, TableLayout,TBody,THead } from './components/shared/table/Table'
+import Login from './components/landingPage/pages/Login'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import AllRoutes from './routes/AllRoutes'
+
 
 const App = () => {
   return (
-    <div></div>
+    <>
+    <BrowserRouter> 
+    <AllRoutes/>
+    </BrowserRouter>
+    </>
   )
 }
 
