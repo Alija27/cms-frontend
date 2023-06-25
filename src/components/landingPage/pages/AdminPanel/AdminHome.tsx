b44eb36a1726e48813db6c4c8e38e1c15ce9758d
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-25 21:03:18 +0545

    Admin page created

---
+++ src/components/landingPage/pages/AdminPanel/AdminHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../shared/dashboard/Layout'
+
+export const AdminHome = () => {
+  return (
+    <Layout>
+        <div className="text-xl text-gray-500">
+            Dashboard
+        </div>
+    </Layout>
+  )
+}
