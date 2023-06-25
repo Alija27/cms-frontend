c57d0d60971eaac04cb20c63319f71d7a4ca753b
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-25 21:00:57 +0545

    added UserSlice in rootreducer

--- src/app/rootReducer.tsx
+++ src/app/rootReducer.tsx
@@ -1,8 +1,10 @@
 import {combineReducers} from "@reduxjs/toolkit"
 import  AuthSlice  from "./feature/Auth/AuthSlice";
+import UserSlice  from "./feature/User/UserSlice";
 
 const rootReducer = combineReducers({
     AuthSlice:AuthSlice,
+    UserSlice:UserSlice,
 });
 
 export default rootReducer;
