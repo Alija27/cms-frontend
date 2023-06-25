8450b9ddd755a788fc9336aebbbea679050d3eb1
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-25 21:04:42 +0545

    Middleware created

--- src/routes/middleware/AdminRoute.tsx
+++ src/routes/middleware/AdminRoute.tsx
@@ -3,14 +3,11 @@ import { useAppSelector } from '../../app/hooks'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const AdminRoute = () => {
-    const authState=useAppSelector((store)=>store.AuthSlice);
-    if(
-        authState.current_user &&
-        authState.current_user.roles.includes("admin")
-    ){
-        return <Outlet/>
-    }else{
-        return <Navigate to="/login"/>
+    const authState = useAppSelector((store) => store.AuthSlice);
+    if (authState.current_user && authState.current_user.roles.includes("admin") ) {
+        return <Outlet />
+    } else {
+        return <Navigate to="/login" />
     }
 }
 
--- src/routes/middleware/AuthRoute.tsx
+++ src/routes/middleware/AuthRoute.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { useAppSelector } from '../../app/hooks'
 import { store } from '../../app/store'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const AuthRoute = () => {
     const authState=useAppSelector((store)=>store.AuthSlice)
+    if(
+        authState.current_user &&
+        authState.current_user.roles.includes("admin")
+    ){
+        return <Navigate to="/admin"/>
+    }else if(
+        authState.current_user &&
+        authState.current_user.roles.includes("accountant")
+    ){
+     return <Navigate to="/accountant"/>   
+    }
+    else{
+        return <Outlet/>
+    }
   
 }
 
-export default AuthRoute
+export default AuthRoute;
