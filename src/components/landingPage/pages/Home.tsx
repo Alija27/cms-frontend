12f86ea8749ff7abd2e2b8b4220d03fccd9371ed
Alija Ghimire <alijaghimire27@gmail.com>
2023-06-29 14:45:28 +0545

    HomePage

---
+++ src/components/landingPage/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import collegeImage from '../../../assets/28466.jpg'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+    <div className="bg-cover bg-center" 
+    style={{ backgroundImage: `url(${collegeImage})`, 
+    height:"45rem"}}>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
