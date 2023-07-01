c7157b1bfb52443040a910cdefcf60e100fa9676
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-01 13:55:19 +0545

    register Slice in reducer

--- src/app/rootReducer.tsx
+++ src/app/rootReducer.tsx
@@ -1,10 +1,14 @@
 import {combineReducers} from "@reduxjs/toolkit"
 import  AuthSlice  from "./feature/Auth/AuthSlice";
 import UserSlice  from "./feature/User/UserSlice";
+import TeacherSlice  from "./feature/Teacher/TeacherSlice";
+import DepartmentSlice from "./feature/Department/DepartmentSlice";
 
 const rootReducer = combineReducers({
     AuthSlice:AuthSlice,
     UserSlice:UserSlice,
+    TeacherSlice:TeacherSlice,
+    DepartmentSlice:DepartmentSlice,
 });
 
 export default rootReducer;
