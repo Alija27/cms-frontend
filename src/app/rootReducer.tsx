6d9fd2c30a8f9b91901ae02264a638a844261e1c
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-05 12:56:21 +0545

    Added semesterSlice

--- src/app/rootReducer.tsx
+++ src/app/rootReducer.tsx
@@ -3,12 +3,14 @@ import  AuthSlice  from "./feature/Auth/AuthSlice";
 import UserSlice  from "./feature/User/UserSlice";
 import TeacherSlice  from "./feature/Teacher/TeacherSlice";
 import DepartmentSlice from "./feature/Department/DepartmentSlice";
+import SemesterSlice from "./feature/Semester/SemesterSlice";
 
 const rootReducer = combineReducers({
     AuthSlice:AuthSlice,
     UserSlice:UserSlice,
     TeacherSlice:TeacherSlice,
     DepartmentSlice:DepartmentSlice,
+    SemesterSlice:SemesterSlice,
 });
 
 export default rootReducer;
