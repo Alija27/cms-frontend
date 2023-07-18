f885922438e8c3d13553383e2f5ee4230f19257c
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-18 09:43:15 +0545

    updated component table done

--- src/components/shared/table/Table.tsx
+++ src/components/shared/table/Table.tsx
@@ -1,50 +1,49 @@
 import React from 'react'
-import Buttons from '../buttons/Buttons'
+import { FaEdit } from "react-icons/fa"
+import { AiFillDelete } from "react-icons/ai"
 
 export const TableLayout = ({
     children,
     heading,
+    rightheading
 
 }: any) => {
     return (
-        <div className="flex flex-col border ">
-            <div className='overflow-x-auto sm:mx-6 lg:mx-8'>
-                <div className='inline-block min-w-full'>
-                    <div className='overflow-hidden'>
-                        <div className='flex justify-between px-1 py-2 text-gray-500'>
-                            <div className="font-semibold text-xl">{heading}</div>
-                            <div> <Buttons type="button" text="Add new"/></div>
-                        </div>
-                        {children}
-                    </div>
-                </div>
+        <div className="flex flex-col border text-gray-500">
+            <div className='flex p-2 justify-between border-b text-gray-500'>
+                <div className="font-semibold text-xl">{heading}</div>
+                <div> {rightheading}</div>
+            </div>
+            <div className='inline-block overflow-auto'>
+                {children}
             </div>
         </div>
     )
 }
 
-export const Table =({children}:any)=>(
-    <table className="min-w-full border border-separate border-gray-100">{children}</table>
+export const Table = ({ children }: any) => (
+    <table className="min-w-full border ">{children}</table>
+);
+
+export const THead = ({ children }: any) => {
+    return <thead className=" text-white ]">{children}</thead>
+}
+export const TBody = ({ children }: any) => {
+    return <tbody className="divide-y-2 
+        divide-gray-200">{children}</tbody>
+}
+
+
+export const TableActions = (props: any,) => {
+    const { onEdit, onDelete,children } = props;
+
+    return (
+        <td className="flex space-x-4">
+            {/* <div className="hover:text-blue-800"><FaEdit size={20} /></div>
+            <div className="hover:text-red-800"><AiFillDelete size={20} /></div> */}
+            {children}
+        </td>
     );
-
-    export const THead=({children}:any)=>{
-        return <thead className="border text-white bg-blue-500">{children}</thead>
-    }
-    export const TBody=({children}:any)=>{
-        return <tbody className="divide-y-2 
-        divide-gray-500">{children}</tbody>
-    }
-    
-
-    export const TableActions = (props: any) => {
-	const { onEdit, onDelete } = props;
-
-	return (
-		<td className="flex">
-			<div>Edit</div>|
-            <div>Delete</div>
-		</td>
-	);
 };
 
 
