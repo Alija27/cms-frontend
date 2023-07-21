f73c3e691773937295ac0e4afecb8fec72be3030
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-21 21:00:17 +0545

    Batch Api and Slice created

---
+++ src/app/feature/Batch/BatchApi.tsx
@@ -0,0 +1,54 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../../../config/axiosInstance";
+export const getAllBatches = createAsyncThunk(
+    "getBatches",
+    async (undefined, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.get("http://localhost:8000/api/batches");
+            return res.data;
+        }
+        catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
+
+export const createBatch = createAsyncThunk(
+    "createBatch",
+    async (data: any, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.post("http://localhost:8000/api/batches", data);
+            return res.data;
+        }
+        catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
+
+export const updateBatch = createAsyncThunk(
+    "updateBatch",
+    async ({ data, id }: any, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.put(`http://localhost:8000/api/batches/${id}`, data);
+            return res.data;
+        }
+        catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
+
+export const deleteBatch = createAsyncThunk(
+    "deleteBatch",
+    async (batch_id: any, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.delete(`http://localhost:8000/api/batches/${batch_id}`);
+            return res.data;
+        }
+        catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
+
---
+++ src/app/feature/Batch/BatchSlice.tsx
@@ -0,0 +1,114 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getAllBatches, createBatch, updateBatch, deleteBatch } from "./BatchApi";
+
+
+export const BatchSlice = createSlice({
+    name: "Batch",
+    initialState: {
+        batches: [],
+        loading: false,
+        error: false,
+        success: false,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getAllBatches.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(getAllBatches.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = false;
+            state.success = true;
+            state.batches = action.payload.data;
+        }
+        );
+
+        builder.addCase(getAllBatches.rejected, (state) => {
+            state.loading = false;
+            state.error = true;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(createBatch.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+            state.success = false;
+        }
+        );
+
+        
+        builder.addCase(createBatch.fulfilled, (state:any, action) => {
+            state.loading = false;
+            state.error = false;
+            state.success = true;
+            state.batches.push(action.payload.data);
+        }
+        );
+
+        builder.addCase(createBatch.rejected, (state) => {
+            state.loading = false;
+            state.error = true;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(updateBatch.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(updateBatch.fulfilled, (state:any, action) => {
+            state.loading = false;
+            state.error = false;
+            state.success = true;
+            state.batches = state.batches.map((batch:any) => {
+                if (batch.id === action.payload.data.id) {
+                    return action.payload.data;
+                }
+                return batch;
+            });
+        }
+        );
+
+        builder.addCase(updateBatch.rejected, (state) => {
+            state.loading = false;
+            state.error = true;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(deleteBatch.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+            state.success = false;
+        }
+        );
+
+        builder.addCase(deleteBatch.fulfilled, (state:any, action) => {
+            state.loading = false;
+            state.error = false;
+            state.success = true;
+            state.batches = state.batches.filter((batch:any) => batch.id !== action.payload.data.id);
+        }
+        );
+
+        builder.addCase(deleteBatch.rejected, (state) => {
+            state.loading = false;
+            state.error = true;
+            state.success = false;
+        }
+        );
+
+    }
+});
+
+export default BatchSlice.reducer;
+        
+        
