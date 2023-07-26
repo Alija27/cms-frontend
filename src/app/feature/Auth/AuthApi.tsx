c64fdf1c6543b73cb9f0d8ba19598c7b7a0827f6
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-26 21:39:44 +0545

    api logout added

--- src/app/feature/Auth/AuthApi.tsx
+++ src/app/feature/Auth/AuthApi.tsx
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../../config/axiosInstance";
+import { error_toast } from "../../../toast";
+import { success_toast } from "../../../toast";
 
 export const login = createAsyncThunk(
     "login",
@@ -9,22 +11,41 @@ export const login = createAsyncThunk(
                 "http://localhost:8000/api/login", data
             );
             console.log(res.data);
+            success_toast(res.data);
             return res.data;
         } catch (err: any) {
             console.log(err);
+            error_toast(err);
             return rejectWithValue(err.response.data);
         }
     }
 );
 
-export const getuser= createAsyncThunk(
+export const getuser = createAsyncThunk(
     "getuser",
-    async(undefined,{rejectWithValue})=>{
-        try{
-            const res=await axiosInstance.get("http://localhost:8000/api/getuser");
+    async (undefined, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.get("http://localhost:8000/api/getuser");
+
+            return res.data;
+            
+        }
+        catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+)
+
+export const logout = createAsyncThunk(
+    "logout",
+    async (undefined, { rejectWithValue }) => {
+        try {
+            const res = await axiosInstance.post("http://localhost:8000/api/logout");
+            success_toast(res.data);
             return res.data;
         }
-        catch(err:any){
+        catch (err: any) {
+            error_toast(err);
             return rejectWithValue(err.response.data);
         }
     }
