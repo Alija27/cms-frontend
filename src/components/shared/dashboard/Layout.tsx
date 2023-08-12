496d45477571c23e3292d125a4158cda74f62c41
Alija Ghimire <alijaghimire27@gmail.com>
2023-08-12 12:04:06 +0545

    added condition in Navlink

--- src/components/shared/dashboard/Layout.tsx
+++ src/components/shared/dashboard/Layout.tsx
@@ -4,16 +4,32 @@ import Dashnav from './Dashnav'
 import TextFields from '../inputs/TextFields'
 import Buttons from '../buttons/Buttons'
 import Dashboard from './Dashboard'
-import { DNavLinks, adminNavLinks } from '../../../constants/Constants'
+import { DNavLinks, accountantNavLinks, adminNavLinks, examinerNavLinks, librarianNavLinks, studentNavLinks, teacherNavLinks } from '../../../constants/Constants'
 import { useAppSelector } from '../../../app/hooks'
 
 const Layout = ({children}:any) => {
   const authState=useAppSelector((store)=>store.AuthSlice);
   let navlinks:DNavLinks[]=[];
-  
+  console.log(authState.current_user?.roles);
   if(authState.current_user && authState.current_user.roles.includes("admin")){
     navlinks=adminNavLinks;
-  }else{
+  }
+  else if(authState.current_user && authState.current_user.roles.includes("librarian")){
+    navlinks=librarianNavLinks;
+  }
+  else if(authState.current_user && authState.current_user.roles.includes("teacher")){
+    navlinks=teacherNavLinks;
+  }
+  else if(authState.current_user && authState.current_user.roles.includes("accountant")){
+    navlinks= accountantNavLinks;
+  }
+  else if(authState.current_user && authState.current_user.roles.includes("student")){
+    navlinks=studentNavLinks;
+  }
+  else if(authState.current_user && authState.current_user.roles.includes("examiner")){
+    navlinks=examinerNavLinks;
+  }
+  else{
     navlinks=[];
   }
   return (
