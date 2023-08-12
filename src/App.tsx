dd282ae39f2b3c36841660e6f600aed1b088be48
Alija Ghimire <alijaghimire27@gmail.com>
2023-08-12 12:08:22 +0545

    Change in Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Navbar from './components/landingPage/navbar/Navbar'
-import Home from './components/landingPage/pages/Home'
-import Sidenav from './components/shared/dashboard/Sidenav'
-import Dashnav from './components/shared/dashboard/Dashnav'
-import Layout from './components/shared/dashboard/Layout'
-import { Table, TableActions, TableLayout,TBody,THead } from './components/shared/table/Table'
-import Login from './components/landingPage/pages/Login'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import AllRoutes from './routes/AllRoutes'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 
 const App = () => {
   return (
     <>
-    <BrowserRouter> 
-    <AllRoutes/>
-    </BrowserRouter>
+    
+    <Provider store={store}>
+      <AllRoutes/>
+      
+     <ToastContainer 
+     autoClose={1000}/>
+  </Provider>
     </>
   )
 }
