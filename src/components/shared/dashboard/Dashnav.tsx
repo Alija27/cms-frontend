5a64951482864428512d998135aac9cadb0efc71
Alija Ghimire <alijaghimire27@gmail.com>
2023-08-17 14:31:33 +0545

    Logout added

--- src/components/shared/dashboard/Dashnav.tsx
+++ src/components/shared/dashboard/Dashnav.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
 import {RxHamburgerMenu} from "react-icons/rx"
-import {HiOutlineUserCircle} from "react-icons/hi"
-import {FcManager} from "react-icons/fc"
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import {useNavigate } from "react-router-dom";
+import { logout } from "../../../app/feature/Auth/AuthApi";
+import Buttons from "../buttons/Buttons";
 const Dashnav = () => {
+  const dispatch = useAppDispatch();
+	const authState: any = useAppSelector((state) => state.AuthSlice);
+  const navigate = useNavigate();
+
+  const logoutUser = async () => {
+		await dispatch(logout()).then(() => {
+			navigate("/login");
+		});
+	};
   return (
     <>
       <div className="bg-white px-2 shadow py-3  flex justify-between mt-1">
@@ -10,7 +20,8 @@ const Dashnav = () => {
           <RxHamburgerMenu size={25}/>
         </div>
         <div>
-         <FcManager size={28} className="border-2  rounded-full "/> 
+      {/*    <FcManager size={28} className="border-2  rounded-full "/> */} 
+         <Buttons onClick={logoutUser} text="Logout" type="submit" className="bg-red-500"/>
         {/* <HiOutlineUserCircle size={28} className="border-2  rounded-full "/> */}
         </div>
       </div>
